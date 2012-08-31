49849f2590b819ca53a6cc2533eab9e2243e7803
Riley Moses <victi.resurgo@gmail.com>
2012-08-31 16:28:45 -0600

    Make application debuggable, support all but small screen sizes

--- LunchList/src/csci498/thevoiceless/lunchlist/LunchList.java
+++ LunchList/src/csci498/thevoiceless/lunchlist/LunchList.java
@@ -4,17 +4,20 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class LunchList extends Activity {
+public class LunchList extends Activity
+{
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lunch_list);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lunch_list);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_lunch_list, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		getMenuInflater().inflate(R.menu.activity_lunch_list, menu);
+		return true;
+	}
 }
