7f5bcb9b51cc7b70a03483aacbd0116eda3a62b2
Riley Moses <victi.resurgo@gmail.com>
2012-08-31 16:38:45 -0600

    Create Restaurant class

---
+++ LunchList/src/csci498/thevoiceless/lunchlist/Restaurant.java
@@ -0,0 +1,27 @@
+package csci498.thevoiceless.lunchlist;
+
+public class Restaurant
+{
+	private String name = "";
+	private String address = "";
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String newName)
+	{
+		this.name = newName;
+	}
+	
+	public String getAddress()
+	{
+		return address;
+	}
+	
+	public void setAddress(String newAddress)
+	{
+		this.address = newAddress;
+	}
+}
