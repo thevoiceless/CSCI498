4ce411c23ab4c22c83ea00b746924c149cf612ac
Riley Moses <victi.resurgo@gmail.com>
2012-09-01 19:28:13 -0600

    Initial commit of ActionBarSherlock test application, see http://avilyne.com/?p=180

---
+++ SherlockTest/src/csci498/thevoiceless/sherlocktest/Preferences.java
@@ -0,0 +1,59 @@
+package csci498.thevoiceless.sherlocktest;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import com.actionbarsherlock.ActionBarSherlock;
+import com.actionbarsherlock.ActionBarSherlock.OnCreateOptionsMenuListener;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.MenuItem.OnMenuItemClickListener;
+
+public class Preferences extends Activity implements OnCreateOptionsMenuListener
+{
+	ActionBarSherlock sherlock = ActionBarSherlock.wrap(this);
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		sherlock.setUiOptions(ActivityInfo.UIOPTION_SPLIT_ACTION_BAR_WHEN_NARROW);
+		// Note: ActionBarSherlock instance handles the content layout
+		sherlock.setContentView(R.layout.preferences);
+	}
+
+	// This onCreateOptionsMenu method is passed a standard android.view.Menu object
+	@Override
+	public boolean onCreateOptionsMenu(android.view.Menu menu)
+	{
+		// Let the ActionBarSherlock object handle the creation of menus
+		return sherlock.dispatchCreateOptionsMenu(menu);
+	}
+	
+	// The ActionBarSherlock object, in turn, will have the parent Activity class
+	// receive an onCreateOptionsMenu() method, but this time the Menu is an
+    // com.actionbarsherlock.view.Menu object
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuItem example1 = menu.add("Example1");
+		example1.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		
+		MenuItem dismiss = menu.add("Dismiss");
+		dismiss.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		
+		dismiss.setOnMenuItemClickListener(new OnMenuItemClickListener()
+		{
+			@Override
+			public boolean onMenuItemClick(MenuItem item)
+			{
+				finish();
+				return true;
+			}
+		});
+		
+		return true;
+	}
+
+}
---
+++ SherlockTest/src/csci498/thevoiceless/sherlocktest/SherlockTesting.java
@@ -0,0 +1,55 @@
+package csci498.thevoiceless.sherlocktest;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.MenuItem.OnMenuItemClickListener;
+
+// Extend SherlockActivity
+public class SherlockTesting extends SherlockActivity
+{
+	//ActionBar actionBar;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sherlock_testing);
+
+		//actionBar = getSupportActionBar();
+	}
+
+	// Note: These are actionbarsherlock.view.Menu objects that
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuItem prefs = menu.add("Preferences");
+		prefs.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
+		prefs.setOnMenuItemClickListener(new OnMenuItemClickListener()
+		{
+			@Override
+			public boolean onMenuItemClick(MenuItem item)
+			{
+				Intent prefIntent = new Intent(SherlockTesting.this,
+						Preferences.class);
+				SherlockTesting.this.startActivity(prefIntent);
+				return true;
+			}
+		});
+
+		// Empty menu items to test display changes in different orientations
+		menu.add("Other Stuff")
+				.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		menu.add("Great Stuff")
+				.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		menu.add("Probably Hidden").setShowAsAction(
+				MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
+		return true;
+	}
+}
