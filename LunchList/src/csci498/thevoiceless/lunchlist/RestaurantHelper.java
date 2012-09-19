f708e4b665db0e5176ac2ea8021b7a3c852d437b
Riley Moses <victi.resurgo@gmail.com>
2012-09-18 18:41:31 -0600

    Implement getters in RestaurantHelper for the various fields

--- LunchList/src/csci498/thevoiceless/lunchlist/RestaurantHelper.java
+++ LunchList/src/csci498/thevoiceless/lunchlist/RestaurantHelper.java
@@ -2,6 +2,7 @@ package csci498.thevoiceless.lunchlist;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -9,6 +10,10 @@ public class RestaurantHelper extends SQLiteOpenHelper
 {
 	private static final String DATABASE_NAME = "lunchlist.db";
 	private static final int SCHEMA_VERSION = 1;
+	private static final int NAME_INT = 1;
+	private static final int ADDR_INT = 2;
+	private static final int TYPE_INT = 3;
+	private static final int NOTES_INT = 4;
 	
 	public RestaurantHelper(Context context)
 	{
@@ -35,5 +40,42 @@ public class RestaurantHelper extends SQLiteOpenHelper
 		cv.put("type", type.toString());
 		cv.put("notes", notes);
 		getWritableDatabase().insert("restaurants", "name", cv);
+	}
+	
+	public Cursor getAll()
+	{
+		return getReadableDatabase().rawQuery("SELECT _id, name, address, type, notes FROM restaurants ORDER BY name", null);
+	}
+	
+	public String getName(Cursor c)
+	{
+		return c.getString(NAME_INT);
+	}
+	
+	public String getAddress(Cursor c)
+	{
+		return c.getString(ADDR_INT);
+	}
+	
+	public Restaurant.Type getType(Cursor c)
+	{
+		String theType = c.getString(TYPE_INT);
+		if(theType.equals("SIT_DOWN"))
+		{
+			return Restaurant.Type.SIT_DOWN;
+		}
+		else if(theType.equals("TAKE_OUT"))
+		{
+			return Restaurant.Type.TAKE_OUT;
+		}
+		else
+		{
+			return Restaurant.Type.DELIVERY;
+		}
+	}
+	
+	public String getNotes(Cursor c)
+	{
+		return c.getString(NOTES_INT);
 	}
 }
