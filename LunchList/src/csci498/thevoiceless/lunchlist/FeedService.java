c0cea052f133b2b588100f3d56269d1a50455703
Riley Moses <victi.resurgo@gmail.com>
2012-10-07 12:19:25 -0600

    Add FeedService to manifest

--- LunchList/src/csci498/thevoiceless/lunchlist/FeedService.java
+++ LunchList/src/csci498/thevoiceless/lunchlist/FeedService.java
@@ -5,9 +5,13 @@ import android.content.Intent;
 
 public class FeedService extends IntentService
 {
+	private static final String NAME = "FeedService";
+	
+	// IntentService requires you to implement a no-argument constructor
+	// and chain to the superclass, supplying a name for your IntentService
 	public FeedService()
 	{
-		super("FeedService");
+		super(NAME);
 	}
 	
 	@Override
