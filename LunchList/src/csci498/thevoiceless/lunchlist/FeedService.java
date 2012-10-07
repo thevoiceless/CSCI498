919588e8afc45dfc2c225d385f57bdc6bb062cf9
Riley Moses <victi.resurgo@gmail.com>
2012-10-07 12:12:07 -0600

    Add FeedService stub

---
+++ LunchList/src/csci498/thevoiceless/lunchlist/FeedService.java
@@ -0,0 +1,19 @@
+package csci498.thevoiceless.lunchlist;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class FeedService extends IntentService
+{
+	public FeedService()
+	{
+		super("FeedService");
+	}
+	
+	@Override
+	public void onHandleIntent(Intent i)
+	{
+		// TODO
+	}
+
+}
