8d2a996fc66ba52613caf8bea952a117fa9d60c6
Riley Moses <victi.resurgo@gmail.com>
2012-10-30 19:06:07 -0600

    Display notification

--- LunchList/src/csci498/thevoiceless/lunchlist/OnAlarmReceiver.java
+++ LunchList/src/csci498/thevoiceless/lunchlist/OnAlarmReceiver.java
@@ -1,16 +1,46 @@
 package csci498.thevoiceless.lunchlist;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class OnAlarmReceiver extends BroadcastReceiver
 {
+	private static final int NOTIFY_ME_ID = 1337;
+	
 	@Override
 	public void onReceive(Context context, Intent intent)
 	{
-		Intent i = new Intent(context, AlarmActivity.class);
-		i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		context.startActivity(i);
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		boolean useNotification = prefs.getBoolean(context.getString(R.string.key_alarm_use_notification), true);
+		
+		if (useNotification)
+		{
+			NotificationManager notificationMgr = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+			Notification notification = new Notification(R.drawable.stat_notify_chat, 
+					context.getString(R.string.notification_lunch_ticker), 
+					System.currentTimeMillis());
+			PendingIntent i = PendingIntent.getActivity(context, 
+					0, 
+					new Intent(context, AlarmActivity.class), 
+					0);
+			notification.setLatestEventInfo(context, 
+					context.getString(R.string.activity_lunchlist), 
+					context.getString(R.string.notification_lunch_content), 
+					i);
+			notification.flags |= Notification.FLAG_AUTO_CANCEL;
+			notificationMgr.notify(NOTIFY_ME_ID, notification);
+		}
+		else
+		{
+			Intent i = new Intent(context, AlarmActivity.class);
+			i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			context.startActivity(i);
+		}
 	}
 }
