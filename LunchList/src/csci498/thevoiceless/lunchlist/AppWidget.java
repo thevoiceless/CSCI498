55ae8c197013ce7ac0dc6bcd457e5be69eada4cb
Riley Moses <victi.resurgo@gmail.com>
2012-11-05 16:38:24 -0700

    Implement onUpdate in AppWidget

--- LunchList/src/csci498/thevoiceless/lunchlist/AppWidget.java
+++ LunchList/src/csci498/thevoiceless/lunchlist/AppWidget.java
@@ -1,8 +1,16 @@
 package csci498.thevoiceless.lunchlist;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
 
 public class AppWidget extends AppWidgetProvider
 {
+	@Override
+	public void onUpdate(Context context, AppWidgetManager manager, int[] appWidgetIds)
+	{
+		context.startService(new Intent(context, WidgetService.class));
+	}
 
 }
